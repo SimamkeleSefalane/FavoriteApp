c1dac816ff8f31c9019b99c89d846e153d29c15a
SimamkeleSefalane <102727091+SimamkeleSefalane@users.noreply.github.com>
2022-05-11 16:30:12 +0200

    Unable to add pictures

---
+++ XamarinFavAppSln/XamarinFavApp/XamarinFavApp/FavoriteTabbedPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XamarinFavApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class FavoriteTabbedPage : TabbedPage
+    {
+        public FavoriteTabbedPage()
+        {
+            InitializeComponent();
+
+            var homePage = new HomePage();
+         //   var file = new FileImageSource();
+         ///   file.File = "HomePageIcon.png";
+            homePage.IconImageSource = "HomePageIcon.png";
+
+
+            Children.Add(homePage);
+
+            var moviesPage = new Movies();
+            //   var file = new FileImageSource();
+            ///   file.File = "HomePageIcon.png";
+            moviesPage.IconImageSource = "moviesIcon.png";
+
+
+            Children.Add(moviesPage);
+            Children.Add(new Series());
+            Children.Add(new Downloads());
+            Children.Add(new More());
+        }
+    }
+}
